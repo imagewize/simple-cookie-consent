89b72b39fad617797cd1dac8a60639b24fb40ab3
Jasper Frumau <jasper@imagewize.com>
2025-03-31 16:23:59 +0700

    Return settings_modal

--- src/index.js
+++ src/index.js
@@ -20,6 +20,43 @@ const defaultConfig = {
                     text: 'Reject all',
                     role: 'accept_necessary'
                 }
+            },
+            settings_modal: {
+                title: 'Cookie settings',
+                save_settings_btn: 'Save settings',
+                accept_all_btn: 'Accept all',
+                reject_all_btn: 'Reject all',
+                close_btn_label: 'Close',
+                cookie_table_headers: [
+                    { col1: 'Name' },
+                    { col2: 'Domain' },
+                    { col3: 'Expiration' },
+                    { col4: 'Description' }
+                ],
+                blocks: [
+                    {
+                        title: 'Cookie usage',
+                        description: 'We use cookies to ensure the basic functionalities of the website and to enhance your online experience. You can choose for each category to opt-in/out whenever you want. For more details, please read our <a href="#privacy-policy" class="cc-link">privacy policy</a>.'
+                    }, 
+                    {
+                        title: 'Strictly necessary cookies',
+                        description: 'These cookies are essential for the proper functioning of my website. Without these cookies, the website would not work properly',
+                        toggle: {
+                            value: 'necessary',
+                            enabled: true,
+                            readonly: true
+                        }
+                    }, 
+                    {
+                        title: 'Performance and Analytics cookies',
+                        description: 'These cookies allow the website to remember the choices you have made in the past',
+                        toggle: {
+                            value: 'analytics',
+                            enabled: false,
+                            readonly: false
+                        }
+                    }
+                ]
             }
         }
     }
